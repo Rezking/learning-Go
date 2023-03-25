7d3e5ca572135656b12b76559423d08dc3a851b9
Onanuga Oreoluwa <onagoreoluwa92@gmail.com>
2023-03-25 12:12:16 +0100

    methods and interfaces

--- hello1.go
+++ hello1.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// arrays, slices, maps, control flow, functions
+type Circle struct {
+	x, y, r float64
+}
+
+type Shapes interface {
+	area() float64
+}
+
+type Rectangle struct {
+	l, w float64
+}
+
+func square(x *int) {
+	*x = *x * *x
+}
 
 func half(x int) (int, bool) {
 	if x%2 == 0 {
@@ -41,6 +61,13 @@ func fib(x int) []int {
 	return slice
 }
 
+func swap(x *int, y *int) {
+	new := *x
+	var ptrx *int = x
+	var ptry *int = y
+	*ptrx = *y
+	*ptry = new
+}
 func makeOddgenerator() func() uint {
 	i := uint(1)
 	return func() (ret uint) {
@@ -50,11 +77,39 @@ func makeOddgenerator() func() uint {
 	}
 }
 
+// working with methods
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func (r *Rectangle) area() float64 {
+	return r.l * r.w
+}
+
+func totalArea(shapes ...Shapes) float64 {
+	var totalarea float64
+	for _, s := range shapes {
+		totalarea += s.area()
+	}
+	return totalarea
+}
+
 func main() {
-	fmt.Println(half(4))
-	fmt.Println(largest(2, 3, 4, 12, 5)) // 12
-	nextOdd := makeOddgenerator()
-	fmt.Println(nextOdd()) // prints 1
-	fmt.Println(nextOdd()) //prints 3
-	fmt.Println(fib(6))
+	// x := 2
+	// y := 6
+	// swap(&x, &y)
+	// fmt.Println(x)
+	// fmt.Println(y)
+	c := Circle{0, 0, 5}
+	fmt.Println(c.area())
+	fmt.Println(c.x)
+	// square(&x)
+	// fmt.Println(x)
+	// fmt.Println(half(4))
+	// fmt.Println(largest(2, 3, 4, 12, 5)) // 12
+	// nextOdd := makeOddgenerator()
+	// fmt.Println(nextOdd()) // prints 1
+	// fmt.Println(nextOdd()) //prints 3
+	// fmt.Println(fib(6))
+
 }
